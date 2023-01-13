39d45134ac9c3cd4a40d05a129cfd3f722ccc069
Lucas Nguyen <lucasoAlt@outlook.com>
2023-01-13 21:01:31 +0100

    update license + edge in inPrivate

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ You shall follow all the terms of the MIT License (./LICENSE).
 """
 
 
-# ----- Requires: beautifulsoup4 == 4.11.1 -----
+# ----- Requires: selenium == 4.7.2 -----
+# ----- Requires: webdriver-manager == 3.8.5 -----
+# ----- Requires: msedge-selenium-tools -----
 
-from bs4 import BeautifulSoup as bs4
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from msedge.selenium_tools import Edge, EdgeOptions
+
+
+driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+
+options = EdgeOptions()
+options.use_chromium = True
+options.add_argument("-inprivate")
+
+driver.set_window_size(1920,1080)
+
+driver.get('https://go.microsoft.com/fwlink/p/?linkid=873020&clcid=0x409&culture=en-us&country=us')
+
+sleep(10)
+
+driver.close()
+driver.quit()
