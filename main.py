7c3e25f990648a7cbf2d5448ffd7166f6ff54e41
Lucas Nguyen <lucasoAlt@outlook.com>
2023-01-13 20:36:04 +0100

    License information

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+"""
+All code in this file is licensed under the MIT License, the whole license and copyright holder(s) is in ./LICENSE.
+You shall follow all the terms of the MIT License (./LICENSE).
+"""
+
+
 # ----- Requires: beautifulsoup4 == 4.11.1 -----
 
+from bs4 import BeautifulSoup as bs4
